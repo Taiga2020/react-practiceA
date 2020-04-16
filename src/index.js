6c96b16259d920a97f39dc01091953c712f56353
Taiga2020 <tech2020future@gmail.com>
2020-04-16 16:33:21 +0900

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const messageStyle = {
+	color: "blue"
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+	return (
+		<>
+			<h2>Reactハンズオン！</h2>
+			<p style={{color: "red" }}>Reactって難しいの？</p>
+			<p style={messageStyle}>Reactは難しくないよー</p>
+		</>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
